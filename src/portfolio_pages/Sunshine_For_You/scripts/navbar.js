265f910c09033d8400ce7ba2f91e913d795af9c4
Issac Liu <issac.liuyt@gmail.com>
2022-02-12 20:05:50 -0500

    Create React

--- src/portfolio_pages/Sunshine_For_You/scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {makeOpaque()};
-
-function makeOpaque() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("nav-bar").style.backgroundColor = "#fffee2";
-      } else {
-        document.getElementById("nav-bar").style.backgroundColor = "";
-      }
-}
