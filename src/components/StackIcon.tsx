d32efaf9cf075c8a0cb10e2c9ff67affc089ace1
Issac Liu <issac.liuyt@gmail.com>
2022-02-13 02:56:21 -0500

    Create Template for StackIcon component

---
+++ src/components/StackIcon.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IStackIconProps {
+}
+
+export default class StackIcon extends React.Component<IStackIconProps> {
+  public render() {
+    return (
+      <div>
+        TEST
+      </div>
+    );
+  }
+}
