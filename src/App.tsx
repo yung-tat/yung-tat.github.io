90a167813f597b9661cb88082d50a68bec160d7a
Issac Liu <issac.liuyt@gmail.com>
2022-02-12 20:08:18 -0500

    Changing message

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <h1>Yo i just set this up chill come back in a bit</h1>
         </p>
         <a
           className="App-link"
