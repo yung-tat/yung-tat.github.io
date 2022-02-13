2872b4f8be859ba47981f59423c22b889cff14d2
Issac Liu <issac.liuyt@gmail.com>
2022-02-13 02:55:58 -0500

    Create StackDropDown button component

---
+++ src/components/StackDropDown.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { BiChevronDown } from "react-icons/bi"
+
+export interface IStackDropDownProps {
+  handleClick: any;
+  stateName: string;
+  label: string;
+}
+
+export default class StackDropDown extends React.Component<IStackDropDownProps> {
+  public render() {
+    return (
+      <button 
+        id={this.props.stateName + "Button"}
+        className="w-full border rounded border-slate-800 flex items-center p-3 font-semibold text-xl"
+        type="button"
+        onClick={this.props.handleClick}
+      >
+        {this.props.label}
+        <BiChevronDown className='ml-auto'/>
+      </button>
+    );
+  }
+}
