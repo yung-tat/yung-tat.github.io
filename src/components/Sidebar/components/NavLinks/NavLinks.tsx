c30aff38dde1cc55d0482f9e2153c02583bdc471
Issac Liu <issac.liuyt@gmail.com>
2023-08-15 22:00:12 -0400

    Fix build

--- src/components/Sidebar/components/NavLinks/NavLinks.tsx
+++ src/components/Sidebar/components/NavLinks/NavLinks.tsx
@@ -10,31 +10,38 @@ interface INavLinks {
 const NavLinks: FC<INavLinks> = ({ disableMargin = false }) => {
   const navigate = useNavigate();
   const location = useLocation();
-  return navlinks.map((navlink) => (
-    <MenuItem
-      sx={(theme) => ({
-        display: "flex",
-        alignContent: "center",
-        marginBottom: disableMargin ? 0 : 2,
-        paddingX: 4,
-        color:
-          location.pathname === navlink.path
-            ? theme.palette.secondary.main
-            : undefined,
-        transition: theme.transitions.create(["background-color", "color"], {
-          easing: theme.transitions.easing.easeInOut,
-          duration: theme.transitions.duration.standard,
-        }),
-      })}
-      onClick={() => navigate(navlink.path)}
-      disableGutters
-    >
-      {navlink.icon}
-      <Typography sx={{ fontSize: "20px", marginLeft: 2, lineHeight: 1 }}>
-        {navlink.title}
-      </Typography>
-    </MenuItem>
-  ));
+  return (
+    <>
+      {navlinks.map((navlink) => (
+        <MenuItem
+          sx={(theme) => ({
+            display: "flex",
+            alignContent: "center",
+            marginBottom: disableMargin ? 0 : 2,
+            paddingX: 4,
+            color:
+              location.pathname === navlink.path
+                ? theme.palette.secondary.main
+                : undefined,
+            transition: theme.transitions.create(
+              ["background-color", "color"],
+              {
+                easing: theme.transitions.easing.easeInOut,
+                duration: theme.transitions.duration.standard,
+              }
+            ),
+          })}
+          onClick={() => navigate(navlink.path)}
+          disableGutters
+        >
+          {navlink.icon}
+          <Typography sx={{ fontSize: "20px", marginLeft: 2, lineHeight: 1 }}>
+            {navlink.title}
+          </Typography>
+        </MenuItem>
+      ))}
+    </>
+  );
 };
 
 export default NavLinks;
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,7 +2,13 @@ import { projects } from "../../constants/projects";
 import ProjectInfo from "./components/ProjectInfo";
 
 const Projects = () => {
-  return projects.map((project) => <ProjectInfo project={project} />);
+  return (
+    <>
+      {projects.map((project) => (
+        <ProjectInfo project={project} />
+      ))}
+    </>
+  );
 };
 
 export default Projects;
